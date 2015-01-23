c8486a5cff7428fbb4798d71d4f8c0b5c5ada33d
Marcel Reboucas <mscreboucas@gmail.com>
2015-01-23 19:07:41 -0300

    Adiciona arquivos de i18n

--- app/bootstrap/Global.java
+++ app/bootstrap/Global.java
@@ -1,7 +1,9 @@
 package bootstrap;
 
+import play.GlobalSettings;
+
 /**
  * Created by arturspindola on 21/01/15.
  */
-public class Global {
+public class Global extends GlobalSettings{
 }
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.i18n.Messages;
 import play.mvc.*;
 
 import views.html.*;
@@ -8,7 +9,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(Messages.get("text.language")));
     }
 
 }
